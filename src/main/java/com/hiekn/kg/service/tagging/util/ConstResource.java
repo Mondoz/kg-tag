52f54f574e28925096f4302b782653a8e30c1a41
Lee <lichunxiao@hiekn.com>
2017-11-29 15:42:39 +0800

    add concept instance property

--- src/main/java/com/hiekn/kg/service/tagging/util/ConstResource.java
+++ src/main/java/com/hiekn/kg/service/tagging/util/ConstResource.java
@@ -1,14 +1,17 @@
 package com.hiekn.kg.service.tagging.util;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * 
@@ -59,7 +62,9 @@ public class ConstResource {
 	//field
 	public static final String FIELDS = props.getProperty("tagging_field");
 	public static final String MAPFIELDS = props.getProperty("field_map");
-	
+	public static final List<Long> CONCEPTLIST = JSONArray.parseArray(props.getProperty("concept"),Long.class);
+	public static final List<Long> INSTANCELIST = JSONArray.parseArray(props.getProperty("instance"),Long.class);
+
 	//
 	// elastic search
 	//
